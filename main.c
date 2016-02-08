6bb1b578d3c12e96ece6c7f2e9daeb9ac7997d23
Guillaume WOODWARD <gwoodwar@e3r3p4.42.fr>
2016-02-08 20:58:23 +0100

    sort av when multifile

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/12 16:58:29 by gwoodwar          #+#    #+#             */
-/*   Updated: 2016/02/08 17:30:03 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/02/08 20:58:06 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,28 @@ static void		info_init(t_info *info)
 {
 	UNSET(info->opt, OPT_ALL);
 	dlst_init(&info->headfile);
+}
+
+static void		save_sort_av(char **av, int ac, int i)
+{
+	char		*tmp;
+	int			j;
+
+	while (i < ac)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+			{
+				tmp = av[i];
+				av[i] = av[j];
+				av[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 int				main(int ac, char **av)
@@ -33,9 +55,12 @@ int				main(int ac, char **av)
 		ft_get_dir(".", &info);
 	else
 	{
+		save_sort_av(av, ac, i);
 		SET(info.opt, OPT_MD);
 		while (i < ac)
 		{
+			ft_putstr(av[i]);
+			ft_putstr(":\n");
 			ft_get_dir(av[i], &info);
 			i++;
 			if (i != ac)
